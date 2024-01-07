85d79a915fbc85418f9d9603948d6f1fd58e1258
Nicro01 <nicro@edu.unifil.br>
2024-01-06 22:44:14 -0300

    User Seeder

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,10 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = "TMKT Londrina";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
